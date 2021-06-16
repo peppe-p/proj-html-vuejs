53093ba1aefc86f339082536dd4418e50d6a3fb1
peppe-p <ombrachiara96@gmail.com>
2021-06-17 00:32:02 +0200

    add dynamic tabs in program div

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,11 +12,15 @@ var app = new Vue({
   data: {
     nav_menu: ["home", "pages", "program", "tickets", "speakers", "papers", "blog", "shotcodes"],
     selectedPage: 0,
-    f_gallery: ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"]
+    f_gallery: ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"],
+    tab_on: 1
   },
   methods: {
     prova: function prova(x) {
       this.selectedPage = x;
+    },
+    setTab: function setTab(x) {
+      this.tab_on = x;
     }
   }
 });
--- src/main.js
+++ src/main.js
@@ -4,11 +4,17 @@ var app = new Vue({
         nav_menu: ["home", "pages", "program", "tickets", "speakers", "papers", "blog", "shotcodes"],
         selectedPage: 0,
         f_gallery: ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"],
+        tab_on: 1,
     },
 
     methods: {
         prova: function (x) {
             this.selectedPage = x
+        },
+
+        setTab: function (x) {
+            this.tab_on = x
         }
     }
 })
+
