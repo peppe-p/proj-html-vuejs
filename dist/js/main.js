7a8e51c3580a50942b920d00cc7de40fe058339b
peppe-p <ombrachiara96@gmail.com>
2021-06-16 16:38:07 +0200

    complete footer with dynamic gallery

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,7 +11,8 @@ var app = new Vue({
   el: '#root',
   data: {
     nav_menu: ["home", "pages", "program", "tickets", "speakers", "papers", "blog", "shotcodes"],
-    selectedPage: 0
+    selectedPage: 0,
+    f_gallery: ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"]
   },
   methods: {
     prova: function prova(x) {
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ var app = new Vue({
     data: {
         nav_menu: ["home", "pages", "program", "tickets", "speakers", "papers", "blog", "shotcodes"],
         selectedPage: 0,
+        f_gallery: ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"],
     },
 
     methods: {
