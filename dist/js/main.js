af430d12160dfca4512d7c0b9a5f62f4a6400bd4
peppe-p <ombrachiara96@gmail.com>
2021-06-17 15:08:16 +0200

    create speaker page with functional button

--- dist/js/main.js
+++ dist/js/main.js
@@ -13,14 +13,62 @@ var app = new Vue({
     nav_menu: ["home", "pages", "program", "tickets", "speakers", "papers", "blog", "shotcodes"],
     selectedPage: 0,
     f_gallery: ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"],
-    tab_on: 1
+    tab_on: 1,
+    speaker_page: 0,
+    speakers: [[{
+      name: "Patrick Spencer",
+      role: "S&P Analyzer",
+      imgURL: "./PREVIEW/img/speaker-6.jpg"
+    }, {
+      name: "Patrick Spencer",
+      role: "S&P Analyzer",
+      imgURL: "./PREVIEW/img/speaker-6.jpg"
+    }, {
+      name: "Patrick Spencer",
+      role: "S&P Analyzer",
+      imgURL: "./PREVIEW/img/speaker-6.jpg"
+    }, {
+      name: "Patrick Spencer",
+      role: "S&P Analyzer",
+      imgURL: "./PREVIEW/img/speaker-6.jpg"
+    }], [{
+      name: "Patrick Spencer",
+      role: "S&P Analyzer",
+      imgURL: "./PREVIEW/img/speaker-5.jpg"
+    }, {
+      name: "Patrick Spencer",
+      role: "S&P Analyzer",
+      imgURL: "./PREVIEW/img/speaker-5.jpg"
+    }, {
+      name: "Patrick Spencer",
+      role: "S&P Analyzer",
+      imgURL: "./PREVIEW/img/speaker-5.jpg"
+    }, {
+      name: "Patrick Spencer",
+      role: "S&P Analyzer",
+      imgURL: "./PREVIEW/img/speaker-5.jpg"
+    }]]
   },
   methods: {
-    prova: function prova(x) {
+    menuIndex: function menuIndex(x) {
       this.selectedPage = x;
     },
     setTab: function setTab(x) {
       this.tab_on = x;
+    },
+    pagePre: function pagePre() {
+      this.speaker_page - 1;
+
+      if (this.speaker_page == 0) {} else {
+        this.speaker_page--;
+      }
+    },
+    pageAfter: function pageAfter() {
+      this.speaker_page + 1;
+
+      if (this.speaker_page == this.speakers.length - 1) {} else {
+        this.speaker_page++;
+      }
     }
   }
 });
--- src/main.js
+++ src/main.js
@@ -5,15 +5,49 @@ var app = new Vue({
         selectedPage: 0,
         f_gallery: ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"],
         tab_on: 1,
+        speaker_page: 0,
+        speakers:
+            [
+                [
+                    { name: "Patrick Spencer", role: "S&P Analyzer", imgURL: "./PREVIEW/img/speaker-6.jpg" },
+                    { name: "Patrick Spencer", role: "S&P Analyzer", imgURL: "./PREVIEW/img/speaker-6.jpg" },
+                    { name: "Patrick Spencer", role: "S&P Analyzer", imgURL: "./PREVIEW/img/speaker-6.jpg" },
+                    { name: "Patrick Spencer", role: "S&P Analyzer", imgURL: "./PREVIEW/img/speaker-6.jpg" },
+                ],
+                [
+                    { name: "Patrick Spencer", role: "S&P Analyzer", imgURL: "./PREVIEW/img/speaker-5.jpg" },
+                    { name: "Patrick Spencer", role: "S&P Analyzer", imgURL: "./PREVIEW/img/speaker-5.jpg" },
+                    { name: "Patrick Spencer", role: "S&P Analyzer", imgURL: "./PREVIEW/img/speaker-5.jpg" },
+                    { name: "Patrick Spencer", role: "S&P Analyzer", imgURL: "./PREVIEW/img/speaker-5.jpg" },
+                ],
+            ],
     },
 
     methods: {
-        prova: function (x) {
+        menuIndex: function (x) {
             this.selectedPage = x
         },
 
         setTab: function (x) {
             this.tab_on = x
+        },
+
+        pagePre: function () {
+            this.speaker_page - 1;
+            if (this.speaker_page == 0) {
+
+            } else {
+                this.speaker_page--;
+            }
+        },
+
+        pageAfter: function () {
+            this.speaker_page + 1;
+            if (this.speaker_page == (this.speakers.length - 1)) {
+
+            } else {
+                this.speaker_page++;
+            }
         }
     }
 })
